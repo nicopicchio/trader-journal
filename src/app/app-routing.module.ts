437535c6924ab198a8f019fbd212cc65026f2bba
Nico Picchio <70696511+nicopicchio@users.noreply.github.com>
2023-03-03 21:02:51 +0000

    login form comp added and router setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register', component: RegistrationFormComponent},
+  {path: 'login', component: LoginFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { RegistrationFormComponent } from './registration-form/registration-form
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationFormComponent,
+    LoginFormComponent,
   ],
   imports: [
     BrowserModule,
