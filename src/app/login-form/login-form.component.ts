8d09a47ca480593a7c2da991e8fa992c0140d218
Nico Picchio <nicolapicchio@gmail.com>
2023-03-06 16:21:55 +0000

    created login form component

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent {
 
+export class LoginFormComponent {
+  title: string = 'Trader Journal';
 }
